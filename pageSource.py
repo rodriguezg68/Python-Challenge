b87c22eb39cd4679a6d8940df3e94ddf6f9cfd2b
rodriguezg68 <rodriguezg68@utexas.edu>
2013-04-16 16:00:07 -0500

    my solution to Challenge 2

--- pageSource.py
+++ pageSource.py
@@ -1,3 +1,11 @@
-file = open('./orc.txt', 'r+')
-print file
-file.read()
+file = open("./orc.txt", "r")
+search = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+array = []
+for line in file:
+	string = line
+	for char in string:
+		for letter in search:
+			if char == letter:
+				array.append(char)
+answer = ''.join(array)
+print answer	
